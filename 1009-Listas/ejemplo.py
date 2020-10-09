f211249f7bcf1828329ab8d12bb7cbcba43bd1da
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2020-10-09 07:34:27 -0500

    ejemplo tarea

---
+++ 1009-Listas/ejemplo.py
@@ -0,0 +1,14 @@
+# Defininen 3 funciones sujeto, verbo y objeto
+def sujeto():
+    return "sujeto"
+
+
+def verbo():
+    return "verbo"
+
+
+def objeto():
+    return "objeto"
+
+
+print(sujeto(),verbo(),objeto())
